0447c09bfd6240a72351a48cee87644178b382f7
obieglok <obieglok@tcd.ie>
2020-06-16 15:21:25 +0100

    Started a form to fill out location you want the weather for

--- weather.js
+++ weather.js
@@ -6,7 +6,32 @@ $.getJSON("https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=me
   var icon="https://openweathermap.org/img/w/" + data.weather[0].icon +".png";
   var temp=Math.round(data.main.temp);
   var weather=data.weather[0].main;
+  var location=data.name;
   $('.weatherpicture').attr('src',icon);
   $('.weather').append(weather);
   $(".temp").append(temp + " C");
+  // console.log(location);
+  $(".location").append(location);
+
+});
+// function locationofPerson()
+// {
+//   var input=document.getElementById("Location").value;
+//   console.log(input);
+//   alert(input);
+// }
+
+const area=document.getElementById("Location");
+const form=document.getElementById("form");
+const errorElement=document.getElementById("error");
+form.addEventListener("submit", (e) => {
+  console.log("here");
+  let messages= [];
+  if(area.value=== '' || area.value===null){
+    messages.push("Location is Required");
+  }
+  if(messages.length>0){
+      e.preventDefault();
+      errorElement.innerText=messages.join(', ');
+  }
 });
